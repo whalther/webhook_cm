5769c21b3a560eff995ece4773e28eaa3a848904
AlvaroPosada <alvaro.posada@onelinkbpo.com>
2020-05-08 15:19:30 -0600

    Prueba pipeline

--- Application/LinkPagosApp.cs
+++ Application/LinkPagosApp.cs
@@ -5,9 +5,7 @@ using Domain.Services;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Application
 {
